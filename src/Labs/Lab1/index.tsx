1d9941901e2993f197c0d056b09a23a674acc11a
Lawrence Osher <lao2205@gmail.com>
2025-01-16 13:43:15 -0500

    headings example

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -2,6 +2,8 @@ export default function Lab1() {
   return (
     <div id="wd-lab1">
       <h2>Lab 1</h2>
+      <h3>Headings</h3>
+      <p>Headings allow you to break up the content into multiple headings</p>
     </div>
   );
 }
