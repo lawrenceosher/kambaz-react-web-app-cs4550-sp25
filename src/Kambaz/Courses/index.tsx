87933e45d00d4eb297d6c6922e9d662c66d2fd27
Lawrence Osher <lao2205@gmail.com>
2025-01-19 18:44:47 -0500

    Adding navigation for courses and dashboard

---
+++ src/Kambaz/Courses/index.tsx
@@ -0,0 +1,35 @@
+import CourseNavigation from "./Navigation";
+import { Navigate, Route, Routes } from "react-router";
+
+export default function Courses() {
+  return (
+    <div id="wd-courses">
+      <h2>Course 1234</h2>
+      <hr />
+      <table>
+        <tr>
+          <td valign="top">
+            <CourseNavigation />
+          </td>
+          <td valign="top">
+            <Routes>
+              <Route path="/" element={<Navigate to="Home" />} />
+              <Route path="Home" element={<h2>Home</h2>} />
+              <Route path="Modules" element={<h2>Modules</h2>} />
+              <Route path="Piazza" element={<h2>Piazza</h2>} />
+              <Route path="Zoom" element={<h2>Zoom</h2>} />
+              <Route path="Assignments" element={<h2>Assignments</h2>} />
+              <Route
+                path="Assignments/:aid"
+                element={<h2>Assignment Editor</h2>}
+              />
+              <Route path="Quizzes" element={<h2>Quizzes</h2>} />
+              <Route path="Grades" element={<h2>Grades</h2>} />
+              <Route path="People" element={<h2>People</h2>} />
+            </Routes>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
+}
