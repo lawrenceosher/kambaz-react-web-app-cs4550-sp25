26f217bcb587ca310ec0244729b4ed30475d4ce2
Lawrence Osher <lao2205@gmail.com>
2025-02-09 15:29:48 -0500

    finished breadcrumbs

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -4,19 +4,20 @@ import AssignmentEditor from "./Assignments/Editor";
 import Home from "./Home";
 import Modules from "./Modules";
 import CourseNavigation from "./Navigation";
-import { Navigate, Route, Routes, useParams } from "react-router";
+import { Navigate, Route, Routes, useParams, useLocation } from "react-router";
 import PeopleTable from "./People/Table";
 import { courses } from "../Database";
 
 export default function Courses() {
   const { cid } = useParams();
   const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
 
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        {course && course.name}
+        {course && course.name} &gt; {pathname.split("/")[4]}
       </h2>
       <hr />
       <div className="d-flex">
