3224b02ae2f5f551b0156b61009e019dc80ca160
Lawrence Osher <lao2205@gmail.com>
2025-02-23 01:14:55 -0500

    finished through boolean state variables section

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,7 +1,30 @@
+import { Container } from "react-bootstrap";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+
 export default function Lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+
   return (
-    <div id="wd-lab4">
+    <Container id="wd-lab4">
       <h3>Lab 4</h3>
-    </div>
+      <ClickEvent />
+
+      <PassingDataOnEvent />
+
+      <PassingFunctions theFunction={sayHello} />
+
+      <EventObject />
+
+      <Counter />
+
+      <BooleanStateVariables />
+    </Container>
   );
 }
