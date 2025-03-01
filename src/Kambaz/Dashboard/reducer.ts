5a659916ab0325a317cb42a1bca4491862e8c384
Lawrence Osher <lao2205@gmail.com>
2025-02-28 22:54:30 -0500

    create reducer for enrollments

---
+++ src/Kambaz/Dashboard/reducer.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { enrollments } from "../Database";
+import { v4 as uuidv4 } from "uuid";
+
+const initialState = {
+  enrollments: enrollments,
+};
+
+const enrollmentsSlice = createSlice({
+  name: "enrollments",
+  initialState,
+  reducers: {
+    addEnrollment: (state, { payload: enrollment }) => {
+      const newEnrollment = {
+        _id: uuidv4(),
+        user: enrollment.user,
+        course: enrollment.course,
+      };
+
+      state.enrollments = [...state.enrollments, newEnrollment];
+    },
+
+    deleteEnrollment: (state, { payload: enrollmentId }) => {
+      state.enrollments = state.enrollments.filter(
+        (enrollment) => enrollment !== enrollmentId
+      );
+    },
+  },
+});
+
+export const { addEnrollment, deleteEnrollment } = enrollmentsSlice.actions;
+export default enrollmentsSlice.reducer;
